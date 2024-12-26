7c5219fbaabda7c548e60568afe902ad4c9f4a0b
Benja-Dev <begalan@outlook.es>
2024-12-25 19:20:50 -0600

    Agregando cliente supabase

---
+++ src/api/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Configuración de Supabase
+const supabaseUrl = 'https://elnsqlogvbumukipeczr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsbnNxbG9ndmJ1bXVraXBlY3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxNDAxMTYsImV4cCI6MjA1MDcxNjExNn0.SpKnOB4EyfoK6k7gjIYB5Tctt7366m4YgnWn6TsrAaA';
+
+// Inicializar el cliente de Supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
