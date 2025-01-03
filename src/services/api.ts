5acde9f00216893ea18c1f5532f57e28e75e3330
Benja-Dev <begalan@outlook.es>
2025-01-03 15:58:51 -0600

    //Actualizacion del proyecto //Se consumme la api de /stores //Se agregó la funcionalidad de Stores (CRUD y activar tiendas)

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,4 +13,17 @@ const fetchDevices = async () => {
   }
 };
 
-fetchDevices();
+const fecthStores = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/stores');
+    console.log('Response data:', response.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching devices:', error.message);
+    } else {
+      console.error('Error fetching devices:', error);
+    }
+  }
+};
+
+fecthStores();
