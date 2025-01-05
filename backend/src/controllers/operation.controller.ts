ae59cf3ec39823f0b5d6fdda6752394e5e9e5582
henvisi-1994 <henvisi1994@gmail.com>
2025-01-05 14:53:57 -0500

    crudOperation

---
+++ backend/src/controllers/operation.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { Operation } from "../models/operation.model";
+import { validateOperation } from "../requests/operation.request";
+import { BaseService } from "../services/base.service";
+import { OperationResource } from "../resources/operation.resource";
+const tableName = 'operations'; // Nombre de la tabla en la base de datos
+export const OperationController = {
+    async getAllOperations(req: Request, res: Response) {
+        try {
+            const where = { ...req.query }; // Convertir los parámetros de consulta en filtros
+            const operations = await BaseService.getAll<Operation>(tableName,['id', 'operation_number', 'operation_type','operation_date','due_date','amount_due','amount_paid','days_overdue','status','judicial_action','created_at','updated_at'],where);
+            res.json(OperationResource.formatOperations(operations));
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async createOperation(req: Request, res: Response) {
+        try {
+            validateOperation(req.body); // Validar los datos
+            const { userId } = req;
+            const operation = await BaseService.create<Operation>(tableName, req.body, userId);
+            res.status(201).json(OperationResource.formatOperation(operation));
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    async updateOperation(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            validateOperation(req.body); // Validar los datos
+            const { userId } = req;
+            const operation = await BaseService.update<Operation>(tableName, parseInt(id), req.body, userId);
+            res.json(OperationResource.formatOperation(operation));
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    async deleteOperation(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const { userId } = req;
+
+            await BaseService.delete<Operation>(tableName, id, userId);
+            res.json({ message: 'Operation eliminada correctamente' });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+}
