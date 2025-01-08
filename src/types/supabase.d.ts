88c90551e5ddb154c3ebf403d6e3207884bfe17d
Benja-Dev <begalan@outlook.es>
2025-01-08 09:00:24 -0600

    Archivo creado por defecto

--- src/types/supabase.d.ts
+++
@@ -1,7 +0,0 @@
-// src/types/supabase.d.ts
-declare module '../api/supabase' {
-    import { SupabaseClient } from '@supabase/supabase-js';
-    const supabase: SupabaseClient;
-    export default supabase;
-  }
-  
