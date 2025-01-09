dbe2d7416981135e7b7487f43c9d14ffa7fd10dc
henvisi-1994 <henvisi1994@gmail.com>
2025-01-08 21:18:58 -0500

    Crud de Usuarios

--- backend/src/routes/usuario.routes.ts
+++ backend/src/routes/usuario.routes.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 
 // // Ruta para obtener todas las categorías
 router.get('/', sessionAuth, UsuarioController.getAllUsuarios);
-
+router.post('/',sessionAuth, UsuarioController.createUsuario);
+router.put('/:id',sessionAuth, UsuarioController.updateUsuario);
+router.delete('/:id',sessionAuth, UsuarioController.deleteUsuario);
 
 export default router;
