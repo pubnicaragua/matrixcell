b776d6375d3becf54cd35daead4f59d6ba067105
Benja-Dev <begalan@outlook.es>
2025-01-14 10:43:40 -0600

    Actualizando archivo de interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 // types.ts
+
 interface ClientListProps {
   clients: Client[];
   setSelectedClient: (client: Client | null) => void;
@@ -6,37 +7,62 @@ interface ClientListProps {
   fetchClientsAndOperations: () => Promise<void>;
 }
 
+export interface Invoice {
+  status: string;
+  created_at: string;
+  operation_id: number;
+}
+
+export interface Device {
+  id: number;
+  imei: string;
+  status: string;
+}
 
 export interface Client {
-    id?: number;
+  id?: number;
+  name: string;
+  phone: string;
+  address: string;
+  city: string;
+  identity_number: string;
+  identity_type: string;
+  due_date: string; //fecha de corte
+  grant_date: string; //fecha de concesión
+  debt_type: string; //tipo de deudor
+  deadline: number; //plazo
+}
+
+export interface Operation {
+  id?: number;
+  operation_number: string;
+  operation_value: number;
+  due_date: string;
+  prox_due_date: string;
+  amount_due: number;
+  amount_paid: number;
+  days_overdue: number;
+  cart_value: number;
+  refinanced_debt: string;
+  judicial_action: number;
+  client_id: number;
+  client?: { // Agrega esta propiedad opcional
+    id: number;
     name: string;
     phone: string;
-    address: string;
-    city: string;
-    identity_number: string;
-    identity_type: string;
-    due_date: string; //fecha de corte
-    grant_date: string; //fecha de concesión
-    debt_type: string; //tipo de deudor
-    deadline: number; //plazo
-  }
-  
-  export interface Operation {
-    id?: number;
-    operation_number: string;
-    operation_value: number;
-    due_date: string;
-    prox_due_date: string;
-    amount_due: number;
-    amount_paid: number;
-    days_overdue: number;
-    cart_value: number;
-    refinanced_debt: string;
-    judicial_action: number;
-    client_id: number;
-    client?: { // Agrega esta propiedad opcional
-        id: number;
-        name: string;
-        phone: string;
-    };
+  };
+}
+
+export interface Dataset {
+  label: string;
+  data: number[];
+  backgroundColor: string[] | string;
+  hoverBackgroundColor: [] | string;
+  borderColor?: string;
+  borderWidth?: number;
+}
+
+export interface ChartData {
+  labels: string[];
+  datasets: Dataset[];
 }
