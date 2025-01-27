b10e52df2b96ee413fb7cf67cf5ce92d91651c40
Benja-Dev <begalan@outlook.es>
2025-01-27 09:43:59 -0600

    Comentario

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,6 +5,8 @@ import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { Card, CardHeader, CardContent, CardFooter } from '../components/ui/card';
 
+
+//comentario
 interface Perfil {
   id: string;
   name: string;
