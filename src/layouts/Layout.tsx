ecae619447d0a4d6d69b4a7ceb4bb6c9f13d62a2
Benja-Dev <begalan@outlook.es>
2025-02-06 08:14:25 -0600

    Funcionalidad de cambio a modo oscuro

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,20 +1,30 @@
-// Layout.js
-import React from 'react';
-import Sidebar from '../components/Sidebar';
+"use client"
 
-import { ReactNode } from 'react';
+// Layout.js
+import { useEffect } from "react"
+import Sidebar from "../components/Sidebar"
+import { useTheme } from "../context/ThemeContext"
+
+import type { ReactNode } from "react"
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const { isDarkMode } = useTheme()
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [isDarkMode])
+
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-background text-foreground">
       <Sidebar />
-      <main className="flex-1 p-5 overflow-scroll">
-        {children}
-      </main>
+      <main className="flex-1 p-5 overflow-scroll">{children}</main>
     </div>
-  );
-};
+  )
+}
 
-
-export default Layout;
+export default Layout
 
